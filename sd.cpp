dbcc839d741c67f396d96bc0f4566407b856a43a
ishsonya <ish.sonya@gmail.com>
2017-05-05 13:44:48 +0300

    Update sd.cpp
    added comments

--- sd.cpp
+++ sd.cpp
@@ -4,14 +4,14 @@
 
 using namespace std;
 fstream fin("input.txt");
-
+// gets m, n, field an returns what you can put in field[mn]
 vector<int> turns(int m, int n, vector<vector<int> > F)
 {
-    vector<int> A(10);
+    vector<int> A(10); //A[number] stores amount of number in hor, vert, sqr
     for (int i = 0; i < 10; i++)
     {
         A[i] = 0;
-}
+    }
     //hor
     for (int j = 0; j < 9; j++)
     {
@@ -25,7 +25,7 @@ vector<int> turns(int m, int n, vector<vector<int> > F)
     //sqr
     int x, y;
     x = (m / 3) * 3;
-    y = (n / 3) * 3;
+    y = (n / 3) * 3; //getting upper left corner of small sqr
     for (int i = x; i < x + 3; i++)
     {
         for (int j = y; j < y + 3; j++)
@@ -43,7 +43,7 @@ vector<int> turns(int m, int n, vector<vector<int> > F)
     }
     return T;
 }
-
+//gets field, returns what you can put in every cell of field
 vector<vector<vector<int> > > priority (vector<vector<int> > F)
 {
     vector<vector<vector<int> > > P;
@@ -59,7 +59,7 @@ vector<vector<vector<int> > > priority (vector<vector<int> > F)
     }
     return P;
 }
-
+//counts how many different numbers you can put in every cell
 vector<vector<int> > coun(vector<vector<vector<int> > > P)
 {
     vector<vector<int> > C;
@@ -83,7 +83,7 @@ bool solve(vector<vector<int> > &F)
     int k = 10;
     int x = 0;
     int y = 0;
-    for (int i = 0; i < 9; i++)
+    for (int i = 0; i < 9; i++) //decides which cell is filled first
     {
         for (int j = 0; j < 9; j++)
         {
@@ -95,24 +95,24 @@ bool solve(vector<vector<int> > &F)
             }
         }
     }
-    if (k == 10)
+    if (k == 10)//if there are no empty cells
     {
         return 1;
     }
     vector<int> T = turns(x, y, F);
-    if (T.size() == 0)
+    if (T.size() == 0)//if the field is not valid already
     {
         return 0;
     }
     for(int i = 0; i < T.size(); i++)
     {
-        F[x][y] = T[i];
+        F[x][y] = T[i];//try T[i]. if works -- we win
         if (solve(F))
         {
             return 1;
         }
     }
-    F[x][y] = 0;
+    F[x][y] = 0;//not a single way 
     return 0;
 }
 
